d4b039b4592e710ed21423d8d3f53959b9421a9a
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-12-07 23:34:46 +0700

    update

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef } from "react";
+import Input from "antd/lib/input/Input";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import "./comment.scss";
 type Props = {
@@ -7,10 +8,12 @@ type Props = {
 };
 
 const Comment = ({ data, tier = 1 }: Props) => {
+  const [showReply, setShowReply] = useState(false);
   const tierRef = useRef(tier);
-  useEffect(() => {
-    console.log(tierRef.current);
-  }, []);
+
+  const handleReply = (item: any) => {
+    setShowReply(!showReply);
+  };
 
   return (
     <>
@@ -21,28 +24,42 @@ const Comment = ({ data, tier = 1 }: Props) => {
 
           return (
             <>
-              <div className="comment">
-                <div className="comment__avatar">
-                  <img
-                    src={
-                      item?.avatar ||
-                      "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"
-                    }
-                    alt=""
-                  />
-                  {tierRef.current && tierRef.current > 1 && (
-                    <span className="comment__avatar-border"></span>
-                  )}
-                </div>
-                <div className="comment__content">
-                  <div className="comment__content-username">
-                    {item?.fullname || ""}
+              <div className="comment-wrapper">
+                <div className="comment">
+                  <div className="comment__avatar">
+                    <img
+                      src={
+                        item?.avatar ||
+                        "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"
+                      }
+                      alt=""
+                    />
+                    {tierRef.current && tierRef.current > 1 && (
+                      <span className="comment__avatar-border"></span>
+                    )}
                   </div>
-                  <div className="comment__content-text">
-                    {item?.content || ""}
+                  <div className="comment__content">
+                    <div className="comment__content-username">
+                      {item?.fullname || ""}
+                    </div>
+                    <div className="comment__content-text">
+                      {item?.content || ""}
+                    </div>
                   </div>
                 </div>
+                <div
+                  className="comment__reply"
+                  onClick={() => handleReply(item)}
+                >
+                  Trả lời
+                </div>
+                {showReply && (
+                  <div className="comment__reply-form">
+                    <Input placeholder="Trả lời ..."></Input>
+                  </div>
+                )}
               </div>
+
               <div>
                 {item?.replies && (
                   <div
