af02b9b883ee69c34f62df44e04da84f9b385277
dpkhang <khangphuc1@gmail.com>
2022-12-11 16:31:27 +0700

    add lohang list

--- src/features/home/pages/index.tsx
+++ src/features/home/pages/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePageHeader from '../components/header/HomePageHeader'
 import HomePage from './home-page/HomePage'
+import HTXPage from './htx'
 import HTXDeatailPage from './htx/detail'
-import RicePage from './rice'
+import LohangPage from './rice'
 import TracingPricePage from './rice/tracing'
 
 const index = () => {
@@ -12,9 +13,10 @@ const index = () => {
       <HomePageHeader></HomePageHeader>
       <Routes>
         <Route path='/' element={<HomePage></HomePage>}></Route>
-        <Route path='/htx' element={<RicePage></RicePage>}></Route>
+        <Route path='/htx' element={<HTXPage></HTXPage>}></Route>
         <Route path='/htx/:id' element={<HTXDeatailPage></HTXDeatailPage>}></Route>
         <Route path='/htx/lohang/:id' element={<TracingPricePage></TracingPricePage>}></Route>
+        <Route path='/lohang' element={<LohangPage></LohangPage>}></Route>
       </Routes>
     </>
   )
